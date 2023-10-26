1bf78569000657c9ef9ca8ffb217f326b1ca8c1d
rogalevska <145899590+rogalevska@users.noreply.github.com>
2023-10-26 18:54:59 +0300

    Update js10_assignment2.js

--- js10_assignment2.js
+++ js10_assignment2.js
@@ -14,12 +14,26 @@ for (let i = 1; i <= 100; i++) {
 }
 
 // Task 2
-let value = 10;
+const value = 10;
 for (let i = 1; i <= value; i++) {
   if (i % 2 !== 0) {
     continue;
   } else if (i % 2 === 0 && typeof i == "number") {
     console.log(i);
+  } else {
+    console.log("Таке чуство шо Бог десь наказує нас за шось");
+  }
+}
+
+// Variant 2
+
+const value2 = 15;
+let a = 1;
+while (a++ < value2) {
+  if (a % 2 !== 0) {
+    continue;
+  } else if (a % 2 === 0 && typeof a == "number") {
+    console.log(a);
   } else {
     console.log("Таке чуство шо Бог десь наказує нас за шось");
   }
