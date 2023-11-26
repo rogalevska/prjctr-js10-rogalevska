4f5bd0af857988c4bb54c73d47d9b462ac0fe2ca
rogalevska <145899590+rogalevska@users.noreply.github.com>
2023-11-26 12:20:10 +0200

    Add files via upload

---
+++ js10_assignment8.js
@@ -0,0 +1,9 @@
+'use strict';
+let navigationDom = document.getElementById('headerTwo');
+console.log(navigationDom);
+let section = document.body.firstElementChild.nextElementSibling;
+console.log(section);
+let paragraph5 = document.querySelector('ul').lastElementChild.previousElementSibling;
+console.log(paragraph5);
+let hatredLevelBlock = document.querySelector('.hatredLevelBlock');
+console.log(hatredLevelBlock);
